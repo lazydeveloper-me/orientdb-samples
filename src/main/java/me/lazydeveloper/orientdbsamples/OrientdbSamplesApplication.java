7e52f6f355bd64e54e43381f5fca60fd99aef322
Varun Addepalli <addepalli.varun@gmail.com>
2018-08-03 07:00:22 -0700

    intitial Commit

---
+++ src/main/java/me/lazydeveloper/orientdbsamples/OrientdbSamplesApplication.java
@@ -0,0 +1,12 @@
+package me.lazydeveloper.orientdbsamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrientdbSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrientdbSamplesApplication.class, args);
+	}
+}
